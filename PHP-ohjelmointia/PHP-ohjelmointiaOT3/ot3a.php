dbc7338c7332078d0b682f949ff56445d34dc85c
niko immonen <niko.immonen@itvelhot.local>
2019-03-12 12:49:30 +0200

    jatkettu ot3

--- PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3a.php
+++ PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3a.php
@@ -5,6 +5,16 @@
     <title>ot3a</title>
 </head>
 <body>
+    <form action="ot3a.php" method="post">
+        <label for="sana"></label>
+        <p>Kirjoita jotain: <input type="text" name="sana"></p>
+        <input type="submit" value="Tulosta">
+    </form>
+
+<?php
+
     
+
+?>
 </body>
 </html>
