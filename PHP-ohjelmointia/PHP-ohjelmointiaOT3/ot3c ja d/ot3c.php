2ea5d1272c6e5051afce5fcdca9b37b03c2f474a
niko immonen <niko.immonen@itvelhot.local>
2019-03-20 12:12:45 +0200

    ot 3 jatkettu

--- PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3c.php
+++ PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3c.php
@@ -5,6 +5,18 @@
     <title>ot3c</title>
 </head>
 <body>
-    
+    <form action="ot3c.php" mathod="POST">
+        <label for="nimi"></label>
+            <p>Nimi: <input type="text" name="nimi"></p>
+        <label for="viesti"></label>
+            <p>Viesti: </p><textarea name="viesti" rows="5" cols="20"></textarea><br>
+            <input type="submit" name="laheta" value="Lähetä">
+    </form> 
+
+<?php
+
+
+
+?>
 </body>
 </html>
