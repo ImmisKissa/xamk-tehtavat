c203fe5bc6a57361cfcebd8e6b6ca125eb6030b9
niko immonen <niko.immonen@itvelhot.local>
2019-03-21 14:09:34 +0200

    ot 3 kohta valmis

--- PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3c.php
+++ PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3c.php
@@ -5,7 +5,7 @@
     <title>ot3c</title>
 </head>
 <body>
-    <form action="ot3c.php" mathod="POST">
+    <form action="ot3c.php" method="POST">
         <label for="nimi"></label>
             <p>Nimi: <input type="text" name="nimi"></p>
         <label for="viesti"></label>
@@ -15,7 +15,34 @@
 
 <?php
 
+if (isset($_POST['nimi']) && (isset($_POST['viesti']))) {
 
+    $nimi = $_POST['nimi'];
+    $viesti = $_POST['viesti'];
+
+    if (strlen($nimi) > 5) {
+        
+    } else {
+        die;
+    }
+
+    if (strlen($viesti) > 5) {
+        
+    } else {
+        die;
+    }
+    
+    htmlspecialchars($nimi);
+    htmlspecialchars($viesti);
+
+    $myfile = fopen("palautteet.txt", "a");
+    $txt = "$nimi\n$viesti\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    
+    $txt = wordwrap($txt,70);
+    // mail("niko.immonen58@gmail.com","Xamk juttu",$txt);
+}
 
 ?>
 </body>
