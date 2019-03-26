1ab60e7fe25ba99cbd4a1a5ce4f488ca00bf8371
niko immonen <niko.immonen@itvelhot.local>
2019-03-26 13:50:00 +0200

    joo

--- PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3d.php
+++ PHP-ohjelmointia/PHP-ohjelmointiaOT3/ot3c ja d/ot3d.php
@@ -12,13 +12,13 @@ $myfile = fopen("palautteet.txt", "r") or die ("Tiedostoa ei voida avata!");
 
 while(!feof($myfile)) {
     $rivi = fgets($myfile);
-}
 
     if(strlen($rivi) > 0) {
         echo (explode(";", $rivi));
         $myfile = "<h1>$rivi[0]</h1>";
         $myfile = "<p>$rivi[1]</p>";
     }
+}
 
 fclose($myfile);
 
