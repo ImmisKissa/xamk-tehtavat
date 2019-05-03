153e07a66776fa48d0748e9ad5e33b1ddec48f33
niko immonen <niko.immonen@itvelhot.local>
2019-05-03 13:07:06 +0300

    jatkettu ot5

--- PHP-ohjelmointia/PHP-ohjelmointiaOT5/ot5e.php
+++ PHP-ohjelmointia/PHP-ohjelmointiaOT5/ot5e.php
@@ -6,5 +6,34 @@
 </head>
 <body>
     
+<form action="" method="POST">
+    <h2>Taulukon päivittäminen</h2>
+        <p>Joukkue 1: <select name="joukkue1" id="joukkue1">
+            <option value="hjk">HJK</option>
+            <option value="kups">KuPs</option>
+            <option value="ilves">Ilves</option>
+            <option value="fc_lahti">FC Lahti</option>
+        </select></p>
+
+        <p>Joukkue 2: <select name="joukkue2" id="joukkue2">
+            <option value="hjk">HJK</option>
+            <option value="kups">KuPs</option>
+            <option value="ilves">Ilves</option>
+            <option value="fc_lahti">FC Lahti</option>
+        </select></p>
+
+        <p>Joukkueen 1 maalit: <input type="text" name="maalit"></p>
+        <p>Joukkueen 2 maalit: <input type="text" name="maalit"></p>
+
+        <input type="submit" value="Päivitä" name="paivita">
+</form>
+
+<?php
+
+include("yhteys.php");
+
+
+
+?>
 </body>
 </html>
